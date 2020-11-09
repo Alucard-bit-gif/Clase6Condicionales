375f4b4b5182057a73a4712893949228d7727879
Sebastián <serviciossociologicos@gmail.com>
2020-11-09 11:13:36 -0500

    Creación archivos html y js

--- Ejercicio6.0/ejercicio6.js
+++ Ejercicio6.0/ejercicio6.js
@@ -1,1 +1,42 @@
-Archivo para condicionales
+'use strict'
+
+let numUno = 5;
+
+Simple(numUno);
+
+function Simple(){
+    if(numUno < 4+2)
+    alert('El condicional es verdadero')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Archivo para condicionales
