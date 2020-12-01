a0bc3c13a413b2e5e05de18d7413f9609e706003
Sebastián <serviciossociologicos@gmail.com>
2020-12-01 18:09:53 -0500

    Se inserto codigo ejemplo 2, pasa a verificacion

--- Ejercicio6.0/If, else, else if, switch/ejercicio6.js
+++ Ejercicio6.0/If, else, else if, switch/ejercicio6.js
@@ -1,18 +1,36 @@
 'use strict'
 
-let numUno = 5;
+//Ejemplo # 1
+
+/*let numUno = 5;
 
 Simple(numUno);
 
 function Simple(){
     if(numUno < 4+2)
     alert('El condicional es verdadero')
+}*/
+
+//------------------------------------------->
+
+//Ejemplo # 2
+
+let numUno = 5;
+
+Simple(numUno);
+
+function Simple(){
+    if(numUno < 4+2){
+    alert('El' + numUno + 'es menor que 4 + 2');
+
+    }
+
+    else {
+
+    alert('El' + numUno + 'es menor que 4 + 2');
+    }
+
 }
-
-
-
-
-
 
 
 
