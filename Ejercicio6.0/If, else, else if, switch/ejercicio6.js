744a4e0c6f29ea28a13180b97df6fc0cc838fe04
Sebastián <serviciossociologicos@gmail.com>
2020-12-01 18:32:37 -0500

    Se inserto codigo ejemplo 3, procede a verificacion

--- Ejercicio6.0/If, else, else if, switch/ejercicio6.js
+++ Ejercicio6.0/If, else, else if, switch/ejercicio6.js
@@ -15,7 +15,7 @@ function Simple(){
 
 //Ejemplo # 2
 
-let numUno = 8;
+/*let numUno = 8;
 
 Simple(numUno);
 
@@ -30,10 +30,30 @@ function Simple(){
     alert('El ' + numUno + ' es mayor que 4 + 2');
     }
 
+}*/
+
+
+//------------------------------------------->
+
+//Ejemplo # 3
+
+let numUno = 8;
+
+Simple(numUno);
+
+function Simple(){
+    if(numUno < 5){
+        alert ('El ' + numUno + 'es menor que 5');
+    }
+
+    else if (numUno == 5){
+        alert ('El ' + numUno + 'es igual a 5')
+    }
+
+    else {
+        alert ('El ' + numUno + 'es mayor a 5')
+    }
 }
-
-
-
 
 
 
