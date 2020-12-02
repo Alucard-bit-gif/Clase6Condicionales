6956d1218ca1902a4b75bb384bb6b1cc2c095aad
Sebastián <serviciossociologicos@gmail.com>
2020-12-01 21:19:52 -0500

    Condicional multiple funciona

--- Ejercicio6.0/If, else, else if, switch/ejercicio6.js
+++ Ejercicio6.0/If, else, else if, switch/ejercicio6.js
@@ -5,14 +5,58 @@
 
 //Declarar variables.
 
-let horasWork = 30;
+let horasWork = 160;
 let horasExtra;
-let valorHora = 400;
-let valorExtra = 500;
+const valorHora = 400;
+const valorExtra = 500;
 let sueldoSem;
+let sueldoExtra;
+let totalaPagar;
 
-        sueldoSem = horasWork*valorHora;
-        alert('El valor a pagar es:' + ' '  +  sueldoSem);
+Calcular (horasWork);
+
+function Calcular()
+{
+    if(horasWork <= 40)  
+    { 
+    sueldoSem = horasWork*valorHora;
+    totalaPagar = sueldoSem;
+    alert('El valor a pagar es:' + ' '  +  totalaPagar);
+    return totalaPagar;
+    }
+
+    else if (horasWork <= 80)
+        {
+        sueldoExtra = (40*valorHora) + ((horasWork - 40)*valorHora);
+        totalaPagar = sueldoExtra;
+        alert('El valor a pagar es:' + ' '  +  totalaPagar);
+        return totalaPagar;
+        }
+
+    else 
+        {
+        sueldoExtra = (80*valorHora) + ((horasWork - 80)*valorExtra);
+        totalaPagar = sueldoExtra;
+        alert('El valor a pagar es:' + ' '  +  totalaPagar);
+        return totalaPagar
+        }
+
+//Leer variables
+
+//Ingresar horas trabajadas
+
+//horasWork = Number(document.getElementById('time').value);//
+
+//Invocar funcion
+    
+sueldoSem = pagoSueldo();
+
+    //Imprimir
+    /*document.getElementById('Inicial').value= valorInicial;    
+    document.getElementById('Descuento').value= valorDescuento;    
+    document.getElementById('Total').value= totalaPagar; */
+    
+}        
    
 
 
