c505c513c0f8d3254fba9f0186b13fc865e88fd1
Sebastián <serviciossociologicos@gmail.com>
2020-12-10 17:58:39 -0500

    Se inentac aplicar codigo, no quiere funcionar

--- Ejercicio6.0/If, else, else if/Ejercicio 6.7/ejercicio 6.7.js
+++ Ejercicio6.0/If, else, else if/Ejercicio 6.7/ejercicio 6.7.js
@@ -1,75 +1,29 @@
 //Ejercicio # 3//
 
-document.getElementById('resultado').style.display="none"
+document.getElementById('resultado').style.display="block";
 
 let numberOne;
 let numberTwo;
 let numberThree;
 let numberMayor;
 
-
-function Registrar(){
-
+function Calcular()
+{
 //Leer variables.
 
 //Ingresar numero.
-numberOne = document.getElementById('nameOne').value;
 
-//Ingresar tipo de contrato.
-tipoContrato = document.getElementById('fijoTemporal').value;
+numberOne = Number(document.getElementById('number1').value);
+numberTwo = Number(document.getElementById('number2').value);
+numberThree = Number(document.getElementById('number3').value);
 
-//
-
-horasWork = Number(document.getElementById('number').value);
 
 //Invocar funcion.
 
-
-salarioNeto = totalPago();
+numberMayor = numberOne  + numberTwo + numberThree;
 
 //Imprimir
 
-document.getElementById('salarioBruto').value = `$ ${salarioBruto}`;    
-document.getElementById('totalDeducciones').value = ` $ ${valorDeduccion}`;
-document.getElementById('totalBonos').value = `$ ${valorBono}`;    
-
-}
-
-function totalPago()
-{
-if(tipoContrato === 'fijo')
-
-{
-salarioBruto = valorHora*horasWork;
-valorDeduccion = salarioBruto*deduccionesMes;
-valorBono = salarioBruto*bonificacionesMes;
-}
-                          
-
-else {
-        salarioBruto = valorHora*horasWork;
-        alert(salarioBruto);
-        return salarioBruto;
-    }
-}
-
-function Calcular()
-{   
-    document.getElementById('survey-form').style.display="none"
-    document.getElementById('resultado').style.display="block"
-
-    if(tipoContrato === 'fijo')
-{
-salarioNeto = (salarioBruto-valorDeduccion) + valorBono;
-alert(salarioNeto);
-document.querySelector('#empleado2').innerHTML = `${nameOne}`;    
-document.querySelector('#pesos2').innerHTML = `$ ${salarioNeto}`;    
-return salarioNeto;
-}
-
-    else {
-        document.querySelector('#empleado2').innerHTML = `${nameOne}`;
-        document.querySelector('#pesos2').innerHTML = `$ ${salarioBruto}`;        
-    }
+alert(numberMayor);
 
 }
