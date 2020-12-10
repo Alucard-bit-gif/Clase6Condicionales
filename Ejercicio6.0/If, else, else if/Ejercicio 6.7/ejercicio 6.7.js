ffb204d6c290842787da11c53d3e571f4c8fecdf
Sebastián <serviciossociologicos@gmail.com>
2020-12-10 18:45:01 -0500

    El codigo funciona

--- Ejercicio6.0/If, else, else if/Ejercicio 6.7/ejercicio 6.7.js
+++ Ejercicio6.0/If, else, else if/Ejercicio 6.7/ejercicio 6.7.js
@@ -1,28 +1,64 @@
 //Ejercicio # 3//
 
+document.getElementById('resultado').style.display="none";
+
 let numeroUno;
 let numeroDos;
 let numeroTres;
 let numeroMayor;
 
 function Calcular(){
-//Leer variables.
+
+
+    document.getElementById('resultado').style.display="block";
+    document.getElementById('survey-form').style.display="none";    
+
+    //Leer variables.
 
 //Ingresar numero.
 
-numeroUno = Number(document.getElementById('number1').value);
-numeroDos = Number(document.getElementById('number2').value);
-numeroTres = Number(document.getElementById('number3').value);
+numeroUno = Number(document.getElementById('numero1').value);
+numeroDos = Number(document.getElementById('numero2').value);
+numeroTres = Number(document.getElementById('numero3').value);
 
 
 
 //Invocar funcion.
 
-numeroMayor = numeroUno  + numeroDos + numeroTres;
+numeroMayor = totalNumero();
 
 //Imprimir
 
-document.querySelector('numeroMayor').innerHTML = numeroMayor;    
+
+document.querySelector('#numeroMayor').innerHTML = numeroMayor;    
+
+}
+
+function totalNumero()
+{
+    if (numeroUno>numeroDos && numeroUno>numeroTres)
+    {
+        numeroMayor = numeroUno;
+        return numeroMayor;
+    }
+
+    else if(numeroDos>numeroUno && numeroDos>numeroTres)
+    {
+        numeroMayor = numeroDos;
+        return numeroMayor;
+    }
+
+    else if(numeroTres>numeroUno && numeroTres>numeroDos)
+    {
+        numeroMayor = numeroTres;
+        return numeroMayor;
+    }
+
+    else
+    {
+        alert('error');
+    }
+
 
 }
 
